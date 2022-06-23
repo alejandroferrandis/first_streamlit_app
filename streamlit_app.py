73f1ec03568a45f4f21ffb3f90cc322f1878f564
Alejandro <alex.petersfeld@gmail.com>
2022-06-23 14:09:32 +0200

    Added pandas
    Added pandas as well as dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
-import streamlit
+import streamlit as st
+import pandas as pd
 
-streamlit.title('My parents New Healthy Diner')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avocado toast')
+st.title('My parents New Healthy Diner')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.header('Breakfast Menu')
+st.text('🥣Omega 3 & Blueberry Oatmeal')
+st.text('🥗Kale, Spinach & Rocket Smoothie')
+st.text('🐔Hard-Boiled Free-Range Egg')
+st.text('🥑🍞Avocado toast')
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.dataframe(my_fruit_list)
