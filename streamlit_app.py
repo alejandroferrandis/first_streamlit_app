12260c49459681a3919b1ce0d55fc8cd31074c7e
Alejandro <alex.petersfeld@gmail.com>
2022-06-23 14:13:49 +0200

    Added interaction
    added interaction to select fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,21 @@ import streamlit as st
 import pandas as pd
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
+
+# TITLE
 st.title('My parents New Healthy Diner')
 
-st.header('Breakfast Menu')
+st.header('Breakfast Favourites')
 st.text('🥣Omega 3 & Blueberry Oatmeal')
 st.text('🥗Kale, Spinach & Rocket Smoothie')
 st.text('🐔Hard-Boiled Free-Range Egg')
 st.text('🥑🍞Avocado toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 st.dataframe(my_fruit_list)
