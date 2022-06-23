0205b72ecf7d01b432a9e38021237d7992839310
Alejandro <alex.petersfeld@gmail.com>
2022-06-23 19:07:00 +0200

    Testing API
    testing API response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import pandas as pd
+import requests
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -21,3 +26,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 st.dataframe(fruits_to_show)
+
+st.text(fruityvice_response)
