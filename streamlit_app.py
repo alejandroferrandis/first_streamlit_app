451beeb566ef10d398bf21170f6c7be550d470bb
Alejandro <alex.petersfeld@gmail.com>
2022-06-24 09:19:21 +0200

    ERROR
    correct typo in streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import snowflake.connector
 
 ######################################################## CONNECTIONS ##########################################################
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
